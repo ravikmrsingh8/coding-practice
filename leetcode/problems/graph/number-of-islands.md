f362208620c3f30bb05aa746d03198b33e6eea3c
ravsing@microsoft.com <ravi.kmr.singh8@gmail.com>
2024-05-27 17:13:24 +0530

    Number of Island optimal solution

--- leetcode/problems/graph/number-of-islands.md
+++ leetcode/problems/graph/number-of-islands.md
@@ -33,7 +33,58 @@ Output: 3
 
 <hr />
 
-### Solution
+### Solution 1
+```java
+class NumIslands {
+    public int numIslands(char[][] grid) {
+        Grid islands = new Grid(grid);
+        return islands.count();
+    }
+
+    private static class Grid {
+        int ROW;
+        int COL;
+        char[][] grid;
+
+        Grid(char[][] grid) {
+            this.grid = grid;
+            ROW = grid.length;
+            COL = grid[0].length;
+        }
+
+        int count() {
+            int N = 0;
+            for (int i = 0; i < ROW; i++) {
+                for (int j = 0; j < COL; j++) {
+                    if (grid[i][j] == '1') {
+                        N++;
+                        dfs(i, j);
+                    }
+                }
+            }
+            return N;
+        }
+
+        void dfs(int i, int j) {
+            grid[i][j] = '2';
+            if (isSafe(i + 1, j)) dfs(i + 1, j);
+            if (isSafe(i - 1, j)) dfs(i - 1, j);
+            if (isSafe(i, j + 1)) dfs(i, j + 1);
+            if (isSafe(i, j - 1)) dfs(i, j - 1);
+        }
+
+        boolean isSafe(int i, int j) {
+            return (i >= 0 && i < ROW)
+                    && (j >= 0 && j < COL)
+                    && grid[i][j] == '1';
+        }
+    }
+}
+
+
+```
+
+### Solution 2
 
 [NumIslands.java](../../src/main/java/org/example/graph/NumIslands.java)
 
--- leetcode/src/main/java/org/example/graph/NumIslands.java
+++ leetcode/src/main/java/org/example/graph/NumIslands.java
@@ -2,11 +2,47 @@ package org.example.graph;
 
 class NumIslands {
     public int numIslands(char[][] grid) {
-        //IslandsBFS islandsByBFS = new IslandsBFS(grid);
-        //return islandsByBFS.count();
+        Grid islands = new Grid(grid);
+        return islands.count();
+    }
 
-        IslandsDFS islandsByDFS = new IslandsDFS(grid);
-        return islandsByDFS.count();
+    private static class Grid {
+        int ROW;
+        int COL;
+        char[][] grid;
+
+        Grid(char[][] grid) {
+            this.grid = grid;
+            ROW = grid.length;
+            COL = grid[0].length;
+        }
+
+        int count() {
+            int N = 0;
+            for (int i = 0; i < ROW; i++) {
+                for (int j = 0; j < COL; j++) {
+                    if (grid[i][j] == '1') {
+                        N++;
+                        dfs(i, j);
+                    }
+                }
+            }
+            return N;
+        }
+
+        void dfs(int i, int j) {
+            grid[i][j] = '2';
+            if (isSafe(i + 1, j)) dfs(i + 1, j);
+            if (isSafe(i - 1, j)) dfs(i - 1, j);
+            if (isSafe(i, j + 1)) dfs(i, j + 1);
+            if (isSafe(i, j - 1)) dfs(i, j - 1);
+        }
+
+        boolean isSafe(int i, int j) {
+            return (i >= 0 && i < ROW)
+                    && (j >= 0 && j < COL)
+                    && grid[i][j] == '1';
+        }
     }
 }
 
